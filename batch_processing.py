f953737f7589ca64ec0702a2c31a8ae646f36205
fan455 <66545913+fan455@users.noreply.github.com>
2022-09-18 09:03:39 +0800

    Add files via upload

---
+++ batch_processing.py
@@ -0,0 +1,22 @@
+import os
+from xxx import xxx
+"""
+xxx is the function used for audio files batch processing.
+"""
+
+input_folder = '01'
+output_folder = '02'
+
+k = 0
+for root, dirs, files in os.walk(input_folder):
+    for file in files:
+        if file.endswith('.wav'):
+            input_path = os.path.join(root, file) 
+            output_path = input_path.replace(input_folder, output_folder)
+            xxx(input_path, output_path)
+            k += 1
+            print(f'{k} files processed.')
+
+print('Normalization completed.')
+
+
---
+++ separate_channels.py
@@ -0,0 +1,11 @@
+import numpy as np
+import soundfile as sf
+
+def separate_channels(input_name, input_folder, output_folder, sf_subtype='PCM_16'):
+    input_path = input_folder + '/' + input_name
+    au, sr = sf.read(input_path)
+    channels_num = au.shape[-1]
+    for i in range(0, channels_num):
+        au_sep = au[:, i]
+        output_path = output_folder + '/' + input_name.replace('.', f'c{i+1}.')
+        sf.write(output_path, au_sep, sr, sf_subtype)
