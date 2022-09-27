973d75b702848360b32d4e453d611fcc90fcb15c
fan455 <66545913+fan455@users.noreply.github.com>
2022-09-27 23:05:08 +0800

    Update separate_channels.py

--- separate_channels.py
+++ separate_channels.py
@@ -1,15 +1,5 @@
 import numpy as np
-import soundfile as sf
 
-def separate_channels(input_name, input_folder, output_folder, sf_subtype='PCM_24'):
-    """
-    It is better to specify the 'sf_subtype' parameter consistent with the bit depth of the input audio file.
-    """
-    input_path = input_folder + '/' + input_name
-    au, sr = sf.read(input_path)
-    channels_num = au.shape[-1]
-    for i in range(0, channels_num):
-        au_sep = au[:, i]
-        output_path = output_folder + '/' + input_name.replace('.', f'c{i+1}.')
-        sf.write(output_path, au_sep, sr, sf_subtype)
-        print(f"Channel {i+1} separated. Written to '{output_path}'.")
+def separate_stereo_channels(au):
+    return au[:, 0], au[:, 1]
+
