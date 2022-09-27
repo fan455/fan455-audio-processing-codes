1b5a2d9d812aecdc2c19f3dc969d7c54f154fa13
fan455 <66545913+fan455@users.noreply.github.com>
2022-09-27 22:49:52 +0800

    Update tfa_test.py

--- tfa_test.py
+++ tfa_test.py
@@ -6,15 +6,18 @@ def get_sine_wave(f=440, phase=0, sr=48000, du=1):
     t = np.arange(0, int(sr*du))/sr
     return np.sin(2*np.pi*f*t + phase)
 
-au, sr = get_sine_wave(phase=np.pi/2), 48000
-#au, sr = sf.read('./audio_input/01.wav')
+#au, sr = get_sine_wave(phase=np.pi/2), 48000
+au, sr = sf.read('./audio_input/01.wav')
 
-f, t, m = tfa.stft(au, sr, 1)
+#f, t, m = tfa.stft(au, sr, 1)
 #f, t, z = tfa.stft(au, sr, 0, 'complex')
 #f, t, r, i = tfa.stft(au, sr, 0, 'r, i')
 
-tfa.plot_stft_m(f, t, m, win_idx=0)
+#tfa.plot_stft_m(f, t, m, win_idx=0)
 #tfa.plot_stft_p(f, t, p, win_idx=0)
 #tfa.plot_stft_z(f, t, z, win_idx=0)
 #tfa.plot_stft_r(f, t, r, win_idx=0)
 #tfa.plot_stft_i(f, t, i, win_idx=0)
+
+f, Pxx = tfa.psd(au, sr, channel=0, nperseg=None, noverlap=None)
+tfa.plot_psd(f, Pxx)
