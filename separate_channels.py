787fa0412336642652765b3b622bbc2bd08220f0
fan455 <66545913+fan455@users.noreply.github.com>
2022-09-29 08:05:13 +0800

    Update separate_channels.py

--- separate_channels.py
+++ separate_channels.py
@@ -1,5 +1,7 @@
 import numpy as np
 
-def separate_stereo_channels(au):
+def separate_2_channels(au):
     return au[:, 0], au[:, 1]
 
+def separate_4_channels(au):
+    return au[:, 0], au[:, 1], au[:, 2], au[:, 3]
