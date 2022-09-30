81f2f5bc86a602fcfaaf9332fb77bfd81358b602
fan455 <66545913+fan455@users.noreply.github.com>
2022-09-30 10:09:08 +0800

    Add files via upload

---
+++ process_channels.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def sep2to2(au):
+    return au[:, 0], au[:, 1]
+
+def sep4to4(au):
+    return au[:, 0], au[:, 1], au[:, 2], au[:, 3]
+
+def sep4to2(au):
+    return au[:, 0:2], au[:, 2:4]
+
+def comb2(au_L, au_R):
+    return np.stack((au_L, au_R), axis=-1)
