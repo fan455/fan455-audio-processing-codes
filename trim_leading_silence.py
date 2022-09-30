96b8878523ee5e5ffb87e74baa4d8319005081cd
fan455 <66545913+fan455@users.noreply.github.com>
2022-09-30 09:57:42 +0800

    Update trim_leading_silence.py

--- trim_leading_silence.py
+++ trim_leading_silence.py
@@ -16,21 +16,23 @@ def trim_leading_silence(au, sr, amp, du, lead_du=None):
         k = 0
         while np.any(bool_mono[k*n: (k+1)*n]) == False:
             k += 1
+            if (k+1)*n > size:
+                raise ValueError('Unable to trim. Probably due to the lack of sound in the lead_du range.')
         trim = k*n
         if trim == 0:
             #print('not trimmed')
             return au
-        elif trim < size:
+        else:
             #print(f'trimmed {round(trim/sr, 4)} seconds')
             return au[trim:]
-        else:
-            raise ValueError('Unable to trim. Probably due to the lack of sound in the lead_du range.')
     elif au.ndim == 2:
         bool_L = np.array(abs(au[0: lead, 0]) - amp > 0) # True is sound. False is silence.
         bool_R = np.array(abs(au[0: lead, 1]) - amp > 0)
         k = 0
         while np.any(bool_L[k*n: (k+1)*n]) == False:
             k += 1
+            if (k+1)*n > size:
+                raise ValueError('Unable to trim. Probably due to the lack of sound in the lead_du range.')
         trim_L = k*n
         k = 0
         while np.any(bool_R[k*n: (k+1)*n]) == False:
@@ -40,10 +42,8 @@ def trim_leading_silence(au, sr, amp, du, lead_du=None):
         if trim == 0:
             #print('not trimmed')
             return au
-        elif trim < size:
+        else:
             #print(f'trimmed {round(trim/sr, 4)} seconds')
             return au[trim:, :]
-        else:
-            raise ValueError('Unable to trim. Probably due to the lack of sound in the lead_du range.')
     else:
         raise ValueError('Only support mono or stereo audio array input')
