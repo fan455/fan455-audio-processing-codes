ba1f4c39a6a1a297068c8b2fc9077376948f5f99
fan455 <66545913+fan455@users.noreply.github.com>
2022-09-30 09:45:07 +0800

    Update trim_leading_silence.py

--- trim_leading_silence.py
+++ trim_leading_silence.py
@@ -1,7 +1,16 @@
 import numpy as np
 
-def trim_leading_silence(au, sr, amp, du, lead_du):
-    n, lead = int(sr*du), int(sr*lead_du)
+def trim_leading_silence(au, sr, amp, du, lead_du=None):
+    """
+    amp: float. abs(au) <= amp is silence.
+    lead_du: float. Seconds at the beginning to analyze. If set to None, use the whole audio.
+    """
+    n = int(sr*du)
+    size = au.shape[0]
+    if lead_du == None:
+        lead = size
+    else:
+        lead = int(sr*lead_du)
     if au.ndim == 1:
         bool_mono = np.array(abs(au[0: lead, 0]) - amp > 0) # True is sound. False is silence.
         k = 0
@@ -9,11 +18,13 @@ def trim_leading_silence(au, sr, amp, du, lead_du):
             k += 1
         trim = k*n
         if trim == 0:
-            print('not trimmed')
+            #print('not trimmed')
             return au
-        else:
-            print(f'trimmed {round(trim/sr, 4)} seconds')
+        elif trim < size:
+            #print(f'trimmed {round(trim/sr, 4)} seconds')
             return au[trim:]
+        else:
+            raise ValueError('Unable to trim. Probably due to the lack of sound in the lead_du range.')
     elif au.ndim == 2:
         bool_L = np.array(abs(au[0: lead, 0]) - amp > 0) # True is sound. False is silence.
         bool_R = np.array(abs(au[0: lead, 1]) - amp > 0)
@@ -27,11 +38,12 @@ def trim_leading_silence(au, sr, amp, du, lead_du):
         trim_R = k*n
         trim = min(trim_L, trim_R)
         if trim == 0:
-            print('not trimmed')
+            #print('not trimmed')
             return au
-        else:
-            print(f'trimmed {round(trim/sr, 4)} seconds')
+        elif trim < size:
+            #print(f'trimmed {round(trim/sr, 4)} seconds')
             return au[trim:, :]
+        else:
+            raise ValueError('Unable to trim. Probably due to the lack of sound in the lead_du range.')
     else:
         raise ValueError('Only support mono or stereo audio array input')
-
