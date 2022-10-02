1f5749b5f1fc4aaad20808ae44f05750aac5a97f
fan455 <66545913+fan455@users.noreply.github.com>
2022-10-02 10:53:54 +0800

    Update note_midi_frequency.py

--- note_midi_frequency.py
+++ note_midi_frequency.py
@@ -1,21 +1,48 @@
+"""
+For pianos' 88 notes only. There're 9 octaves (2 incomplete octaves at both ends), counting from 0.
+"""
 
-def get_midi(note_idx, octave_idx):
-    """
-    For pianos' 88 notes only. There're 9 octaves (2 incomplete at both ends), counting from 0.
-    """
+Note = ('C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B')
+
+def note2midi(note_str, middle_c='C4'):
+    if len(note_str) == 2:
+        note = note_str[0]
+    elif len(note_str) == 3:
+        note = note_str[0: 2]
+    else:
+        raise ValueError('The length of note_str needs to be 2 or 3')
+    note = note.upper()
+    note_idx = Note.index(note)
+    octave_idx = int(note_str[-1]) + 4 - int(middle_c[-1])
+    midi = idx2midi(note_idx, octave_idx)
+    return midi
+
+def midi2note(midi, middle_c='C4'):
+    note_idx, octave_idx = midi2idx(midi)
+    note = Note[note_idx]
+    octave = octave_idx + int(middle_c[-1]) - 4
+    return f'{note}{octave}'
+
+def idx2midi(note_idx, octave_idx):
+
     midi = 12 + note_idx + 12*octave_idx
     if 21 <= midi <= 108:
         return midi
     else:
         return 0
 
-def get_midi_idx(midi):
+def midi2idx(midi):
     """
     For pianos' 88 notes only.
     """
     a, b = divmod(midi-11, 12)
     return b-1, a # note_idx, octave_idx
 
-def get_midi_frequency(midi):
+def midi2freq(midi):
     f = 440*np.exp2((midi-69)/12)
+    return f
+
+def note2freq(note_str):
+    midi = note2midi(note_str)
+    f = midi2freq(midi)
     return f
