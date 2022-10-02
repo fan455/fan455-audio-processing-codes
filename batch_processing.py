f9ef9c779dc8397350d92907d31e96e98c12619b
fan455 <66545913+fan455@users.noreply.github.com>
2022-10-02 11:58:58 +0800

    Update batch_processing.py

--- batch_processing.py
+++ batch_processing.py
@@ -1,9 +1,9 @@
+"""
+Audio batch processing template.
+"""
 import os
 import soundfile as sf
-from xxx import xxx
-"""
-xxx is the function used for audio files batch processing.
-"""
+from xxx import xxx #xxx is the function used for audio files batch processing.
 
 input_folder = '01'
 output_folder = '02'
@@ -14,12 +14,11 @@ for root, dirs, files in os.walk(input_folder):
         if file.endswith('.wav'):
             input_path = os.path.join(root, file) 
             output_path = input_path.replace(input_folder, output_folder)
+            #output_path = output_path.replace('?', 'wav')
             y, sr = sf.read(input_path)
-            y = xxx(y, other_parameters)
+            y = xxx(y)
             sf.write(output_path, y, sr, subtype='PCM_24')
             k += 1
             print(f'{k} files processed.')
 
 print('Batch processing completed.')
-
-
