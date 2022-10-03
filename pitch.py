213d5a6b8ebeee2c8121019dfb9401d98bb354d5
fan455 <66545913+fan455@users.noreply.github.com>
2022-10-03 08:37:49 +0800

    Update pitch.py

--- pitch.py
+++ pitch.py
@@ -1,71 +1,115 @@
 """
-Musical pitch calculations including note, midi, frequency and cent conversions.
-For pianos' 88 notes only. There're 9 octaves (2 incomplete octaves at both ends), counting from 0.
-The frequency calculation follows the standard frequencies of musical notes.
+Audio Time-Frequency Analysis codes based on numpy, scipy and matplotlib.
 """
 import numpy as np
+from scipy import signal
+from scipy.interpolate import CubicSpline
 
-Note = ('C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B')
-
-def note2midi(note_str, middle_c='C4'):
-    if len(note_str) == 2:
-        note = note_str[0]
-    elif len(note_str) == 3:
-        note = note_str[0: 2]
+def psd(au, sr, channel=0, nperseg=None, noverlap=None):
+    if au.ndim == 1:
+        pass
+    elif au.ndim == 2:
+        au = au[:, channel]
     else:
-        raise ValueError('The length of note_str needs to be 2 or 3')
-    note = note.upper()
-    note_idx = Note.index(note)
-    octave_idx = int(note_str[-1]) + 4 - int(middle_c[-1])
-    midi = idx2midi(note_idx, octave_idx)
-    return midi
+        raise ValueError('The input audio array has no dimension, or over 2 dimensions which means it may be a framed audio.')
+    if nperseg == None:
+        nperseg = sr
+    f, Pxx = signal.welch(au, fs=sr, nperseg=nperseg, noverlap=noverlap)
+    return f, Pxx
 
-def midi2note(midi, middle_c='C4'):
-    note_idx, octave_idx = midi2idx(midi)
-    note = Note[note_idx]
-    octave = octave_idx + int(middle_c[-1]) - 4
-    return f'{note}{octave}'
-
-def idx2midi(note_idx, octave_idx):
-    midi = 12 + note_idx + 12*octave_idx
-    if 21 <= midi <= 108:
-        return midi
+def stft(au, sr, channel=0, output='m', nperseg=None, noverlap=0):
+    """
+    Parameters:
+    au: numpy.ndarray. Need to have 1 or 2 dimensions like normal single-channel or multi-channel audio. win_idxd audio needs to be converted to non-win_idxd audio first using other functions to have the right stft.
+    sr: int. Sample rate of au.
+    channel: int. If au has 2 dimensions, which channel to do stft. Defaults to 0 which represents the first channel. 
+    output: str. 'm' will return magnitudes array (zero or positive real values). 'm, p' will return two magnitudes and phases arrays. 'z' will return a complex array as scipy. 'r, i' will return two real and imaginary arrays derived from the complex array.
+    nperseg: None or int. As scipy.signal.stft. None will use the sample rate, int is as scipy.signal.stft.
+    noverlap: None or int. As scipy.signal.stft. None will use half of nperseg.
+    
+    Returns:
+    f: as scipy.signal.stft returns.
+    t: as scipy.signal.stft returns.
+    m: if output='m'.
+    m, p: if output='m, p'.
+    z: if output='z'.
+    z.real, z.imag: if output='r, i'.
+    """
+    if au.ndim == 1:
+        pass
+    elif au.ndim == 2:
+        au = au[:, channel]
     else:
-        return 0 # Only indices of the 88 notes will have true corresponding midi, otherwise returns 0.
-
-def midi2idx(midi):
-    a, b = divmod(midi-11, 12)
-    return b-1, a # note_idx, octave_idx
-
-def midi2freq(midi):
-    f = 440*np.exp2((midi-69)/12)
-    return f
-
-def note2freq(note_str, middle_c='C4'):
-    midi = note2midi(note_str, middle_c=middle_c)
-    f = midi2freq(midi)
-    return f
-
-def freq2cent(f1, f2):
-    cent = 1200*np.log2(f2/f1)
-    return cent
-
-def midi2cent(midi1, midi2):
-    f1, f2 = midi2freq(midi1), midi2freq(midi2)
-    cent = freq2cent(f1, f2)
-    return cent
-
-def ratio2cent(ratio):
-    cent = 1200*np.log2(ratio)
-    return cent
+        raise ValueError('The input audio array has no dimension, or over 2 dimensions which means it may be a framed audio.')
+    if nperseg == None:
+        nperseg = sr
+    if noverlap == None:
+        noverlap = nperseg/2
+    f, t, z = signal.stft(au, fs=sr, nperseg=nperseg, noverlap=noverlap, boundary=None)
+    t = np.around(t, 2)
+    if output == 'm':
+        m = np.abs(z)
+        return f, t, m
+    elif output == 'm, p':
+        m = np.abs(z)
+        p = np.angle(z*np.exp((np.pi/2)*1.0j))
+        return f, t, m, p
+    elif output == 'z':
+        return f, t, z
+    elif output == 'r, i':
+        return f, t, z.real, z.imag
+    else:
+        raise ValueError('Parameter "output" has to be "m, p", "z" or "r, i".')
 
 def cent2ratio(cent):
-    ratio = np.exp2(cent/1200)
-    return ratio
+    return np.exp2(cent/1200)
 
-def pitch_shift_st(f, st):
-    # pitch shift in semitones (st).
-    return f*cent2ratio(st*100)
+def get_pitch_given(au, sr, channel=0, du=None, given_freq=440, given_cent=100, cent_step=1):
+    """
+    Detect the pitch of audio (specifically piano single note) given a pitch, cent band and cent step, using discrete time fourier transform in limited frequency range.
 
-def pitch_shift_cent(f, cent):
-    return f*cent2ratio(cent)
+    Parameters:
+    au: ndarray (float between -1 and 1). The input audio.
+    sr: int. Sample rate of audio.
+    channel: int. The index of the audio channel to analyze. Only supports 1-channel analysis.
+    du: None or float (seconds). The duration of audio to be analyzed. If set to None, it will be the maxinum integar seconds available.
+    given_freq: float (Hz).
+    given_cent: positive float (cent). Half of the cent band around the given frequency for pitch detection.
+    cent_step: float (cent). The distance between Fourier transform's frequencies measured in cents.
+    """
+    if au.ndim == 1:
+        pass
+    elif au.ndim == 2:
+        au = au[:, channel]
+    else:
+        raise ValueError('The input audio array has no dimension, or over 2 dimensions which means it may be a framed audio.')
+    if du == None:
+        t_size = sr*(au.size//sr)
+    else:
+        t_size = int(sr*du)
+    au = au[0: t_size]
+    t = np.arange(0, t_size)/sr
+    F = given_freq*cent2ratio(np.arange(-given_cent, given_cent+1, cent_step))
+    F_size = F.size
+    M = np.empty(0)
+    for i in range(0, F_size):
+        f = F[i]
+        m = np.abs(np.average(au*np.exp(-2*np.pi*f*1.0j*t)))
+        M = np.append(M, m)
+    pitch = F[np.argmax(M)]
+    print(f'{round(pitch, 2)}Hz is the detected pitch given {round(given_freq, 2)}Hz, {round(given_cent, 2)} cent band and {np.round(cent_step, 2)} cent step.')
+    return pitch
+
+def interpolate_pitch(f, num):
+    """
+    Interpolate a frequency array.
+
+    Parameters:
+    f: ndarray (Hz). The input frequency array, strictly increasing.
+    num: int. Number of frequencies to interpolate between every 2 adjacent input frequencies.
+    """
+    size = (num + 1)*f.size - num
+    n, n_f = np.arange(0, size), np.arange(0, size, num+1)
+    cs = CubicSpline(n_f, f)
+    f_itp = cs(n)
+    return f_itp
