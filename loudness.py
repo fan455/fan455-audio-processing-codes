c719e5653818801632ef9a5c5723b80b39abc195
fan455 <66545913+fan455@users.noreply.github.com>
2022-11-02 20:28:08 +0800

    Update loudness.py

--- loudness.py
+++ loudness.py
@@ -7,7 +7,7 @@
 # Special thanks to these authors!
 # I just rewrote some codes to enable short-term and momentary loudness calculations and normalizations for more convinent batch processing of audio files.
 # True peak algorithm has not been implemented here.
-# To get the integrated lufs, use the 'get_Ilufs()' function. To get the short-term or momentary lufs, use the 'get_Mlufs()' function.
+# To get the integrated lufs, use the 'Ilufs_meter().get()' function. To get the short-term or momentary lufs, use the 'Mlufs_meter().get()' function.
 # To normalize the integrated lufs, use the 'norm_Ilufs()' function. To normalize the short-term or momentary lufs, use the 'norm_Mlufs()' function.
 # Both mono and stereo input audio arrays with amplitudes between -1 and 1 are supported.
 
@@ -19,6 +19,10 @@ def amp2db(amp: float): # zero or positive amp value range between 0 and 1.
 
 def db2amp(db: float): # zero or negative db value.
     return np.power(10, db/20)
+
+def check_clipping(au):
+    if np.amax(np.abs(au)) >= 1:
+        raise ValueError('Clipping has occurred.')
 
 def get_sinewave(f, phase=0, A=1, du=1, sr=48000, stereo=True, ls=None, ts=None):
     """
@@ -42,230 +46,194 @@ def get_sinewave(f, phase=0, A=1, du=1, sr=48000, stereo=True, ls=None, ts=None)
         return np.broadcast_to(y, (size, 2))
     else:
         return y
+    
+def norm_lufs(au, sr, old, new=-18.0):
+    return au*db2amp(new - old)
 
-def get_prefilter_coeff(sr, G, Q, fc, filter_type, passband_gain=1.0):
-    """
-    coefficients calculation by csteinmetz1.
-    sr: float. Sampling rate in Hz.
-    G : float. Gain of the filter in dB.
-    Q : float. Q of the filter.
-    fc : float. Center frequency of the shelf in Hz.
-    filter_type: str. Shape of the filter.
-    """    
-    A  = np.power(10, G/40.0)
-    w0 = 2.0*np.pi*(fc/sr)
-    alpha = np.sin(w0)/(2.0*Q)
-    if filter_type == 'high_shelf':
-        b0 =      A * ( (A+1) + (A-1) * np.cos(w0) + 2 * np.sqrt(A) * alpha )
-        b1 = -2 * A * ( (A-1) + (A+1) * np.cos(w0)                          )
-        b2 =      A * ( (A+1) + (A-1) * np.cos(w0) - 2 * np.sqrt(A) * alpha )
-        a0 =            (A+1) - (A-1) * np.cos(w0) + 2 * np.sqrt(A) * alpha
-        a1 =      2 * ( (A-1) - (A+1) * np.cos(w0)                          )
-        a2 =            (A+1) - (A-1) * np.cos(w0) - 2 * np.sqrt(A) * alpha
-    elif filter_type == 'high_pass':
-        b0 =  (1 + np.cos(w0))/2
-        b1 = -(1 + np.cos(w0))
-        b2 =  (1 + np.cos(w0))/2
-        a0 =   1 + alpha
-        a1 =  -2 * np.cos(w0)
-        a2 =   1 - alpha
-    elif filter_type == 'peaking':
-        b0 =   1 + alpha * A
-        b1 =  -2 * np.cos(w0)
-        b2 =   1 - alpha * A
-        a0 =   1 + alpha / A
-        a1 =  -2 * np.cos(w0)
-        a2 =   1 - alpha / A
-    return np.array([b0, b1, b2])/a0, np.array([a0, a1, a2])/a0
+def norm_Mlufs(au, sr, old, new=-18.0):
+    return au*db2amp(new - old)
 
-def get_prefilter_coeff_Kw0(sr=48000):
-    """
-    coefficients in the original ITU documentation.
-    """
-    if sr == 48000:
-        a0 = 1
-        a1 = -1.69065929318241
-        a2 = 0.73248077421585
-        b0 = 1.53512485958697
-        b1 = -2.69169618940638
-        b2 = 1.19839281085285
-        d0, c0 = np.array([b0,b1,b2]), np.array([a0,a1,a2])
+def norm_Ilufs(au, sr, old, new=-23.0):
+    return au*db2amp(new - old)
 
-        a0 = 1
-        a1 = -1.99004745483398
-        a2 = 0.99007225036621
-        b0 = 1.0
-        b1 = -2.0
-        b2 = 1.0
-        d1, c1 = np.array([b0,b1,b2]), np.array([a0,a1,a2])
-        return d0, c0, d1, c1
-    else:
-        raise ValueError(f'Sample rate {sr} is not supported in this function.')
+class Mlufs_meter():
+    # This allows the pre-computation of prefilter coefficients for better performance.
+    def __init__(self, sr):
+        self.sr = sr
+        if sr == 48000:
+            self.d0 = np.array([1.53512485958697, -2.69169618940638, 1.19839281085285])
+            self.c0 = np.array([1.0 , -1.69065929318241, 0.73248077421585])
+            self.d1 = np.array([1.0, -2.0, 1.0])
+            self.c1 = np.array([1.0, -1.99004745483398, 0.99007225036621])
+        elif sr == 44100:
+            self.d0 = np.array([1.5308412300498355, -2.6509799951536985, 1.1690790799210682])
+            self.c0 = np.array([1.0, -1.6636551132560204, 0.7125954280732254])
+            self.d1 = np.array([1.0, -2.0, 1.0])
+            self.c1 = np.array([1.0, -1.9891696736297957, 0.9891990357870394])
+        else:
+            # coefficients calculation by BrechtDeMan. This is closer to the ITU documentation than get_prefilter_coeff_Kw1(sr).
+            # pre-filter 1
+            f0 = 1681.9744509555319
+            G  = 3.99984385397
+            Q  = 0.7071752369554193
+            K  = np.tan(np.pi * f0 / sr) 
+            Vh = np.power(10.0, G / 20.0)
+            Vb = np.power(Vh, 0.499666774155)
+            a0_ = 1.0 + K / Q + K * K
+            b0 = (Vh + Vb * K / Q + K * K) / a0_
+            b1 = 2.0 * (K * K -  Vh) / a0_
+            b2 = (Vh - Vb * K / Q + K * K) / a0_
+            a0 = 1.0
+            a1 = 2.0 * (K * K - 1.0) / a0_
+            a2 = (1.0 - K / Q + K * K) / a0_
+            self.d0, self.c0 = np.array([b0, b1, b2]), np.array([a0, a1, a2])
+            # pre-filter 2
+            f0 = 38.13547087613982
+            Q  = 0.5003270373253953
+            K  = np.tan(np.pi * f0 / sr)
+            a0 = 1.0
+            a1 = 2.0 * (K * K - 1.0) / (1.0 + K / Q + K * K)
+            a2 = (1.0 - K / Q + K * K) / (1.0 + K / Q + K * K)
+            b0 = 1.0
+            b1 = -2.0
+            b2 = 1.0
+            self.d1, self.c1 = np.array([b0, b1, b2]), np.array([a0, a1, a2])
+            
+    def prefilter(self, au):
+        au = signal.lfilter(self.d0, self.c0, au, axis=0)
+        au = signal.lfilter(self.d1, self.c1, au, axis=0)
+        return au
 
-def get_prefilter_coeff_Kw1(sr):
-    # coefficients calculation by csteinmetz1.
-    d0, c0 = get_prefilter_coeff(sr, 4.0, 1/np.sqrt(2), 1500.0, 'high_shelf')
-    d1, c1 = get_prefilter_coeff(sr, 0.0, 0.5, 38.0, 'high_pass')
-    return d0, c0, d1, c1
+    def get(self, au, T=0.4, overlap=0.75, cut_start=None):
+        """
+        Get the maxinum momentary lufs of a mono or stereo audio input. The audio array will be padded zeros at the end to complete the last window.
+        T: float, seconds. Time length of each window. You can change it to 3 to get the short-term lufs (Slufs).
+        overlap: float, proportion. Proportion of overlapping between windows.
+        cut_start: float, seconds. The start seconds to only analyze.
+        Only works for mono or stereo audio because I just summed all the channels and didn't calculate the different weights in case of a 5-channel audio input.
+        You can modify the return 'np.amax(Mlufs)' to 'Mlufs' so you can get the full Mlufs array corresponding to the windowed audio.
+        """
+        step, hop = int(self.sr*T), int(self.sr*T*(1-overlap))
+        Mlufs = np.empty(0)
+        if au.ndim == 2:
+            if cut_start:
+                au = au[0: int(sr*cut_start), :]
+            q1, q2 = divmod(au.shape[0], hop)
+            q3 = step - hop - q2
+            if q3 > 0:
+                au = np.append(au, np.zeros((q3, au.shape[-1])), axis=0)
+            for i in range(0, q1):
+                au_f = au[i*hop: i*hop+step, :]
+                au_f = self.prefilter(au_f)
+                mlufs = -0.691 + 10*np.log10(np.sum(np.average(np.square(au_f), axis=0)))
+                Mlufs = np.append(Mlufs, mlufs)
+        elif au.ndim == 1:
+            if cut_start:
+                au = au[0: int(sr*cut_start)]
+            q1, q2 = divmod(au.shape[0], hop)
+            q3 = step - hop - q2
+            if q3 > 0:
+                au = np.append(au, np.zeros(q3), axis=0)
+            for i in range(0, q1):
+                au_f = au[i*hop: i*hop+step]
+                au_f = self.prefilter(au_f)
+                mlufs = -0.691 + 10*np.log10(np.average(np.square(au_f)))
+                Mlufs = np.append(Mlufs, mlufs)
+        else:
+            raise ValueError(f'au.ndim = {au.ndim} is not supported.')
+        return np.amax(Mlufs)
+    
+class Ilufs_meter():
+    # This allows the pre-computation of prefilter coefficients for better performance.
+    def __init__(self, sr):
+        self.sr = sr
+        if sr == 48000:
+            self.d0 = np.array([1.53512485958697, -2.69169618940638, 1.19839281085285])
+            self.c0 = np.array([1.0 , -1.69065929318241, 0.73248077421585])
+            self.d1 = np.array([1.0, -2.0, 1.0])
+            self.c1 = np.array([1.0, -1.99004745483398, 0.99007225036621])
+        elif sr == 44100:
+            self.d0 = np.array([1.5308412300498355, -2.6509799951536985, 1.1690790799210682])
+            self.c0 = np.array([1.0, -1.6636551132560204, 0.7125954280732254])
+            self.d1 = np.array([1.0, -2.0, 1.0])
+            self.c1 = np.array([1.0, -1.9891696736297957, 0.9891990357870394])
+        else:
+            # coefficients calculation by BrechtDeMan. This is closer to the ITU documentation than get_prefilter_coeff_Kw1(sr).
+            # pre-filter 1
+            f0 = 1681.9744509555319
+            G  = 3.99984385397
+            Q  = 0.7071752369554193
+            K  = np.tan(np.pi * f0 / sr) 
+            Vh = np.power(10.0, G / 20.0)
+            Vb = np.power(Vh, 0.499666774155)
+            a0_ = 1.0 + K / Q + K * K
+            b0 = (Vh + Vb * K / Q + K * K) / a0_
+            b1 = 2.0 * (K * K -  Vh) / a0_
+            b2 = (Vh - Vb * K / Q + K * K) / a0_
+            a0 = 1.0
+            a1 = 2.0 * (K * K - 1.0) / a0_
+            a2 = (1.0 - K / Q + K * K) / a0_
+            self.d0, self.c0 = np.array([b0,b1,b2]), np.array([a0,a1,a2])
+            # pre-filter 2
+            f0 = 38.13547087613982
+            Q  = 0.5003270373253953
+            K  = np.tan(np.pi * f0 / sr)
+            a0 = 1.0
+            a1 = 2.0 * (K * K - 1.0) / (1.0 + K / Q + K * K)
+            a2 = (1.0 - K / Q + K * K) / (1.0 + K / Q + K * K)
+            b0 = 1.0
+            b1 = -2.0
+            b2 = 1.0
+            self.d1, self.c1 = np.array([b0,b1,b2]), np.array([a0,a1,a2])
+            
+    def prefilter(self, au):
+        au = signal.lfilter(self.d0, self.c0, au, axis=0)
+        au = signal.lfilter(self.d1, self.c1, au, axis=0)
+        return au
 
-def get_prefilter_coeff_Kw2(sr):
-    # coefficients calculation by BrechtDeMan. This is closer to the ITU documentation than get_prefilter_coeff_Kw1(sr).
-    # pre-filter 1
-    f0 = 1681.9744509555319
-    G  = 3.99984385397
-    Q  = 0.7071752369554193
-    K  = np.tan(np.pi * f0 / sr) 
-    Vh = np.power(10.0, G / 20.0)
-    Vb = np.power(Vh, 0.499666774155)
-    a0_ = 1.0 + K / Q + K * K
-    b0 = (Vh + Vb * K / Q + K * K) / a0_
-    b1 = 2.0 * (K * K -  Vh) / a0_
-    b2 = (Vh - Vb * K / Q + K * K) / a0_
-    a0 = 1.0
-    a1 = 2.0 * (K * K - 1.0) / a0_
-    a2 = (1.0 - K / Q + K * K) / a0_
-    d0, c0 = np.array([b0,b1,b2]), np.array([a0,a1,a2])
-    # pre-filter 2
-    f0 = 38.13547087613982
-    Q  = 0.5003270373253953
-    K  = np.tan(np.pi * f0 / sr)
-    a0 = 1.0
-    a1 = 2.0 * (K * K - 1.0) / (1.0 + K / Q + K * K)
-    a2 = (1.0 - K / Q + K * K) / (1.0 + K / Q + K * K)
-    b0 = 1.0
-    b1 = -2.0
-    b2 = 1.0
-    d1, c1 = np.array([b0,b1,b2]), np.array([a0,a1,a2])
-    return d0, c0, d1, c1
-
-def get_prefilter_coeff_Fenton(sr):
-    # another coefficients calculation by csteinmetz1, based on Fenton's work.
-    d0, c0 = get_prefilter_coeff(sr, 5.0, 1/np.sqrt(2), 1500.0, 'high_shelf')
-    d1, c1 = get_prefilter_coeff(sr, 0.0, 0.5, 130.0, 'high_pass')
-    d2, c2 = get_prefilter_coeff(sr, 0.0, 1/np.sqrt(2), 500.0, 'high_pass')
-    return d0, c0, d1, c1, d2, c2
-
-def prefilter_Kw(au, sr, d0, c0, d1, c1):
-    au = signal.lfilter(d0, c0, au, axis=0)
-    au = signal.lfilter(d1, c1, au, axis=0)
-    return au   
-
-def prefilter_Fenton(au, sr, d0, c0, d1, c1, d2, c2):
-    au = signal.lfilter(d0, c0, au, axis=0)
-    au = signal.lfilter(d1, c1, au, axis=0)
-    au = signal.lfilter(d2, c2, au, axis=0)
-    return au    
-
-def get_lufs_mono(au, sr):
-    """
-    The input audio should have been prefiltered. Only works for mono or stereo audio because I didn't calculate the different weights (only sum) for the 5-channel input.
-    """
-    lufs = -0.691 + 10*np.log10(np.average(np.square(au)))
-    return lufs
-
-def get_lufs_multi(au, sr):
-    """
-    The input audio should have been prefiltered. Only works for mono or stereo audio because I didn't calculate the different weights (only sum) for the 5-channel input.
-    """
-    lufs = -0.691 + 10*np.log10(np.sum(np.average(np.square(au), axis=0)))
-    return lufs
-
-def get_Mlufs(au, sr, T=0.4, overlap=0.75, cut_start=None):
-    """
-    Get the maxinum momentary lufs of a mono or stereo audio input. The audio array will be padded zeros at the end to complete the last window.
-    T: float, seconds. Time length of each window. You can change it to 3 to get the short-term lufs (Slufs).
-    overlap: float, proportion. Proportion of overlapping between windows.
-    cut_start: float, seconds. The start seconds to only analyze.
-    Only works for mono or stereo audio because I just summed all the channels and didn't calculate the different weights in case of a 5-channel audio input.
-    You can modify the return 'np.amax(Mlufs)' to 'Mlufs' so you can get the full Mlufs array corresponding to the windowed audio.
-    """
-    step, hop = int(sr*T), int(sr*T*(1-overlap))
-    Mlufs = np.empty(0)
-    d0, c0, d1, c1 = get_prefilter_coeff_Kw2(sr)
-    if au.ndim == 2:
-        if cut_start:
-            au = au[0: int(sr*cut_start), :]
-        q1, q2 = divmod(au.shape[0], hop)
-        q3 = step - hop - q2
-        if q3 > 0:
-            au = np.append(au, np.zeros((q3, au.shape[-1])), axis=0)
-        for i in range(0, q1):
-            au_f = au[i*hop: i*hop+step, :]
-            au_f = prefilter_Kw(au_f, sr, d0, c0, d1, c1)
-            mlufs = get_lufs_multi(au_f, sr)
-            Mlufs = np.append(Mlufs, mlufs)
-    elif au.ndim == 1:
-        if cut_start:
-            au = au[0: int(sr*cut_start)]
-        q1, q2 = divmod(au.shape[0], hop)
-        q3 = step - hop - q2
-        if q3 > 0:
-            au = np.append(au, np.zeros(q3), axis=0)
-        for i in range(0, q1):
-            au_f = au[i*hop: i*hop+step]
-            au_f = prefilter_Kw(au_f, sr, d0, c0, d1, c1)
-            mlufs = get_lufs_mono(au_f, sr)
-            Mlufs = np.append(Mlufs, mlufs)
-    else:
-        raise ValueError(f'au.ndim = {au.ndim} is not supported.')
-    return np.amax(Mlufs)
-
-def get_Ilufs(au, sr, T=0.4, overlap=0.75, cut_start=None):
-    """
-    Get the integrated lufs of a mono or stereo audio input. The audio array will be padded zeros at the end to complete the last window.
-    T: float, seconds. Time length of each window. It means the same as 'gating' in the ITU doc.
-    overlap: float, proportion. Proportion of overlapping between windows.
-    Only works for mono or stereo audio because I just summed all the channels and didn't calculate the different weights in case of a 5-channel audio input.
-    """
-    step, hop = int(sr*T), int(sr*T*(1-overlap))
-    Lk, Z = np.empty(0), np.empty(0)
-    d0, c0, d1, c1 = get_prefilter_coeff_Kw2(sr)
-    if au.ndim == 2:
-        if cut_start:
-            au = au[0: int(sr*cut_start), :]
-        nchannel = au.shape[-1]
-        q1, q2 = divmod(au.shape[0], hop)
-        q3 = step - hop - q2
-        if q3 > 0:
-            au = np.append(au, np.zeros((q3, nchannel)), axis=0)       
-        for i in range(0, q1):
-            au_f = au[i*hop: i*hop+step, :]
-            au_f = prefilter_Kw(au_f, sr, d0, c0, d1, c1)
-            z = np.sum(np.average(np.square(au_f), axis=0))
-            lk = -0.691 + 10*np.log10(z)
-            Lk, Z = np.append(Lk, lk), np.append(Z, z)
-    elif au.ndim == 1:
-        if cut_start:
-            au = au[0: int(sr*cut_start)]
-        q1, q2 = divmod(au.shape[0], hop)
-        q3 = step - hop - q2
-        if q3 > 0:
-            au = np.append(au, np.zeros(q3), axis=0)
-        for i in range(0, q1):
-            au_f = au[i*hop: i*hop+step]
-            au_f = prefilter_Kw(au_f, sr, d0, c0, d1, c1)
-            z = np.average(np.square(au_f), axis=0)
-            lk = -0.691 + 10*np.log10(z)
-            Lk, Z = np.append(Lk, lk), np.append(Z, z)
-    else:
-        raise ValueError(f'au.ndim = {au.ndim} is not supported.')
-    Z0 = Z[Lk > -70]
-    td = -0.691 + 10*np.log10(np.average(Z0)) - 10
-    Z = Z[Lk > td]
-    Ilufs = -0.691 + 10*np.log10(np.average(Z))
-    return Ilufs
-
-def norm_Ilufs(au, sr, target=-23.0, cut_start=None):
-    au *= db2amp(target - get_Ilufs(au, sr, cut_start=cut_start))
-    return au
-
-def norm_Mlufs(au, sr, target=-18.0, cut_start=None):
-    au *= db2amp(target - get_Mlufs(au, sr, cut_start=cut_start))
-    return au
-
-def check_clipping(au):
-    if np.amax(np.abs(au)) >= 1:
-        raise ValueError('Clipping has occurred.')
+    def get(self, au, T=0.4, overlap=0.75, cut_start=None):
+        """
+        Get the integrated lufs of a mono or stereo audio input. The audio array will be padded zeros at the end to complete the last window.
+        T: float, seconds. Time length of each window. It means the same as 'gating' in the ITU doc.
+        overlap: float, proportion. Proportion of overlapping between windows.
+        Only works for mono or stereo audio because I just summed all the channels and didn't calculate the different weights in case of a 5-channel audio input.
+        """
+        step, hop = int(self.sr*T), int(self.sr*T*(1-overlap))
+        Lk, Z = np.empty(0), np.empty(0)
+        if au.ndim == 2:
+            if cut_start:
+                au = au[0: int(sr*cut_start), :]
+            nchannel = au.shape[-1]
+            q1, q2 = divmod(au.shape[0], hop)
+            q3 = step - hop - q2
+            if q3 > 0:
+                au = np.append(au, np.zeros((q3, nchannel)), axis=0)       
+            for i in range(0, q1):
+                au_f = au[i*hop: i*hop+step, :]
+                au_f = self.prefilter(au_f)
+                z = np.sum(np.average(np.square(au_f), axis=0))
+                lk = -0.691 + 10*np.log10(z)
+                Lk, Z = np.append(Lk, lk), np.append(Z, z)
+        elif au.ndim == 1:
+            if cut_start:
+                au = au[0: int(sr*cut_start)]
+            q1, q2 = divmod(au.shape[0], hop)
+            q3 = step - hop - q2
+            if q3 > 0:
+                au = np.append(au, np.zeros(q3), axis=0)
+            for i in range(0, q1):
+                au_f = au[i*hop: i*hop+step]
+                au_f = self.prefilter(au_f)
+                z = np.average(np.square(au_f), axis=0)
+                lk = -0.691 + 10*np.log10(z)
+                Lk, Z = np.append(Lk, lk), np.append(Z, z)
+        else:
+            raise ValueError(f'au.ndim = {au.ndim} is not supported.')
+        Z0 = Z[Lk > -70]
+        td = -0.691 + 10*np.log10(np.average(Z0)) - 10
+        Z = Z[Lk > td]
+        Ilufs = -0.691 + 10*np.log10(np.average(Z))
+        return Ilufs
 
 def print_peak(au):
     au_abs = np.abs(au)
@@ -309,3 +277,4 @@ def change_LR_peak_ratio(au, scale):
         ratio = peak_R/peak_L
         au *= np.array([1, (1+(ratio-1)*scale)/ratio])
     return au
+
