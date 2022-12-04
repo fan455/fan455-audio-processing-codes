b98c629f7ceea784b2dea03f57b9adb58fc4b53b
fan455 <66545913+fan455@users.noreply.github.com>
2022-12-04 12:17:39 +0800

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,8 +1,15 @@
+"""
+fast plot
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 def plot_pdf(x, title='probability density function', x_label='x', y_label='density', mycolor='#D1DDC5'):
+    """
+    Plot the probability density function of x.
+    x: 1d array.
+    """
     sns.set(rc={'axes.facecolor':mycolor, 'axes.edgecolor':'grey', 'figure.facecolor':mycolor})
     ax = sns.kdeplot(data=x)
     ax.set_title(title)
@@ -13,8 +20,7 @@ def plot_pdf(x, title='probability density function', x_label='x', y_label='dens
 
 def plot_xy(x, y, title='title', x_label='x', y_label='y', mycolor='#D1DDC5'):
     """
-    x, y: 1d ndarrays with the same sizes.
-    fast plot of x and y
+    x, y: 1d arrays with the same size.
     """
     fig, ax = plt.subplots()
     fig.patch.set_facecolor(mycolor)
@@ -27,6 +33,11 @@ def plot_xy(x, y, title='title', x_label='x', y_label='y', mycolor='#D1DDC5'):
     plt.show()
 
 def plot_au_mono(au, sr, title='title', mycolor='#D1DDC5'):
+    """
+    Plot mono audio array.
+    au: 1d audio array.
+    sr: sample rate.
+    """
     t = np.arange(0, au.size)/sr
     fig, ax = plt.subplots()
     fig.patch.set_facecolor(mycolor)
@@ -40,9 +51,8 @@ def plot_au_mono(au, sr, title='title', mycolor='#D1DDC5'):
 
 def plot_y(y, title='title', x_label='x', y_label='y', mycolor='#D1DDC5'):
     """
-    x is the index of y.
-    x, y: 1d ndarrays with the same sizes.
-    fast plot of x and y
+    Plot array y, x representing the indices of y.
+    y: 1d array.
     """
     x = np.arange(0, y.size)
     fig, ax = plt.subplots()
