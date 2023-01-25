5625b4873d5c45c981e10b6d7d32553985b2dab0
fan455 <66545913+fan455@users.noreply.github.com>
2023-01-25 12:59:34 +0800

    Update tfa_svmd.py

--- tfa_svmd.py
+++ tfa_svmd.py
@@ -30,7 +30,7 @@ def abs2(x):
     #return np.square(x.real) + np.square(x.imag)
     return x.real**2 + x.imag**2 # This line seems faster than the above line.
 
-def svmd(y, out_thr=1e-5, in_thr=1e-10, out_iter_max=9, in_iter_max=50, alpha=1e+1, beta=1e-1, return_type='modes'):
+def svmd(y, out_thr=1e-5, in_thr=1e-10, out_iter_max=9, in_iter_max=50, alpha=1, beta=1e+1, return_type='modes'):
     """
     Parameters:
     y: 1d real array. The input signal array, need to be 1d, real, and better within range [-1, 1].
@@ -91,73 +91,6 @@ def svmd(y, out_thr=1e-5, in_thr=1e-10, out_iter_max=9, in_iter_max=50, alpha=1e
         print(f'The {k}th outer iteration took {i} inner iterations.')
         print(f'f0 = {f0}, fc = {round(fc, 2)}', '\n')
         Modes.append(mode_next)
-        z -= mode_next
-        if np.sum(abs2(z)) <= out_thr:
-            break
-        
-    print(f'Totally {k+1} Modes decomposed.')
-    Modes.append(z)
-    Modes = np.append(np.array(Modes), np.zeros((k+1, y_size//2)), axis=1)
-    Modes = np.real(scipy.fft.ifft(Modes, axis=1, norm='backward')) # transform output back to time domain.
-    if not input_size_is_odd:
-        Modes = np.delete(Modes, -1, axis=1) # delete the last element of output to compensate.
-    print('The last element of output is deleted because input size is even.', '\n')
-    assert Modes.shape[1] == y_size, f'Modes.shape[1] = {Modes.shape[1]}'
-    end_time = timeit.default_timer()
-    print(f'SVMD completed, running time: {round((end_time-start_time), 4)} seconds.', '\n')
-    if return_type == 'modes':
-        return Modes
-    elif return_type == 'modes, residual':
-        return Modes[:-1, :], Modes[-1, :]
-    else:
-        raise ValueError(f'return_type "{return_type}" is not supported.')
-
-def svmd_refined(y, out_thr=1e-5, in_thr=1e-10, out_iter_max=9, in_iter_max=50, alpha=1e+1, beta=1e-1, return_type='modes'):
-    print('SVMD started.', '\n')
-    start_time = timeit.default_timer()
-    print('Input information:')
-    assert y.ndim == 1, 'y.ndim = {y.ndim}'
-    y_size = y.size
-    print(f'y.size = {y_size}')
-    if y_size % 2 != 0:
-        input_size_is_odd = True # no adjustment.
-    else:
-        input_size_is_odd = False
-        y = np.append(y, 0.0)
-        # make input size odd because even fft size will result in a frequency that is both positive and negative.
-        print('The input is padded 1 zero at the end because its size is even.')
-    print(f'input_size_is_odd = {input_size_is_odd}', '\n')
-
-    print('Decomposition information:')
-    z = 2*scipy.fft.rfft(y, axis=0, norm='backward') # transform input to frequency domain. z represents complex.
-    print(f'z.size = {z.size}', '\n')
-    z_idx = np.arange(z.size)
-    Modes, Fc = [], []
-    
-    for k in range(1, out_iter_max+1):
-        f0 = np.argmax(np.abs(z))
-        mode_prev = np.zeros(z.size, dtype=complex)
-        #mode_prev[f0] = abs(z[f0])
-        mode_prev[f0] = z[f0] # I'm not sure if this line or the above line is correct.
-        
-        for i in range(1, in_iter_max+1):
-            mode_prev_sq = abs2(mode_prev)
-            fc = np.sum(z_idx*mode_prev_sq)/np.sum(mode_prev_sq)
-            z_prev = z - mode_prev
-            z_prev_sq = abs2(z_prev)
-            fc_res = np.sum(z_idx*z_prev_sq)/np.sum(z_prev_sq)
-            mode_next = (z*(1 + beta*np.square(z_idx-fc_res)))/ \
-                        (1+alpha*np.square(z_idx-fc) + beta*np.square(z_idx-fc_res))
-            #if np.sum(np.square(np.abs(mode_next)-np.abs(mode_prev))) > in_thr:
-            if np.sum(abs2(mode_next-mode_prev)) > in_thr: # I'm not sure if this line or the above line is correct.
-                mode_prev = mode_next.copy()
-            else:
-                break
-
-        print(f'The {k}th outer iteration took {i} inner iterations.')
-        print(f'f0 = {f0}, fc = {round(fc, 2)}', '\n')
-        Modes.append(mode_next)
-        Fc.append(fc)
         z -= mode_next
         if np.sum(abs2(z)) <= out_thr:
             break
