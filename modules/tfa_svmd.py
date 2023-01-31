9501953950e49f6ae9b5ce776788cd655ff1e3db
fan455 <66545913+fan455@users.noreply.github.com>
2023-01-31 12:01:16 +0800

    Update tfa_svmd.py

--- modules/tfa_svmd.py
+++ modules/tfa_svmd.py
@@ -31,7 +31,7 @@ def abs2(x):
     #return np.square(x.real) + np.square(x.imag)
     return x.real**2 + x.imag**2 # This line seems faster than the above line.
 
-def svmd(y, out_thr=1e-5, in_thr=1e-10, out_iter_max=9, in_iter_max=50, alpha=1, beta=1e-2, return_type='modes, residual'):
+def svmd(y, out_thr=1e-5, in_thr=1e-10, out_iter_max=9, in_iter_max=50, alpha=1, beta=0.5e-2, return_type='modes, residual'):
     """
     Parameters:
     y: 1d real array. The input signal array, need to be 1d, real, and better within range [-1, 1].
@@ -108,7 +108,7 @@ def svmd(y, out_thr=1e-5, in_thr=1e-10, out_iter_max=9, in_iter_max=50, alpha=1,
     else:
         raise ValueError(f'return_type "{return_type}" is not supported.')
 
-def svmd_refined(y, out_thr=1e-5, in_thr=1e-10, out_iter_max=9, in_iter_max=50, alpha=1, beta=1e-2, merge_range=1.5, return_type='modes, residual'):
+def svmd_refined(y, out_thr=1e-5, in_thr=1e-10, out_iter_max=9, in_iter_max=50, alpha=1, beta=0.5e-2, merge_range=1.5, return_type='modes, residual'):
     """
     SVMD including a refinement process to determine the mode number and merge modes with close center frequencies.
     You will be prompted a input requirement to determine the mode number after you have analyzed the normalized distances between modes.
