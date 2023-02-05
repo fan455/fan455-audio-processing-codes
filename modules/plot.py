ee07a1972f7d0d02a2ee78209bd5035064e009f2
fan455 <66545913+fan455@users.noreply.github.com>
2023-02-05 22:07:55 +0800

    Update plot.py

--- modules/plot.py
+++ modules/plot.py
@@ -21,7 +21,8 @@ def plot(y, x=None, title='title', xlabel='x', ylabel='y', grid=True, bgcolor='#
         ax.grid(color='grey', linewidth='1', linestyle='-.')
     plt.show()
 
-def plot_xint(y, x=None, title='title', xlabel='x', ylabel='y', grid=True, annotate_x=False, bgcolor='#D1DDC5'):
+def plot_xint(y, x=None, title='title', xlabel='x', ylabel='y', \
+              grid=True, annotate_x=False, bgcolor='#D1DDC5'):
     """
     x, y: 1d arrays with the same size.
     """
@@ -31,7 +32,8 @@ def plot_xint(y, x=None, title='title', xlabel='x', ylabel='y', grid=True, annot
         x = np.arange(y.size)
     else:
         assert x.dtype == int
-    ax.plot(x, y, color='tab:grey', linestyle='--', marker='.', markersize=12.0, mec='black', mfc='black')
+    ax.plot(x, y, color='tab:grey', linestyle='--', \
+            marker='.', markersize=12.0, mec='black', mfc='black')
     if annotate_x:
         for i in range(x.size):
             plt.annotate(f'{x[i]}', (x[i], y[i]), fontsize='small')
@@ -44,7 +46,8 @@ def plot_xint(y, x=None, title='title', xlabel='x', ylabel='y', grid=True, annot
         ax.grid(color='grey', linewidth='1', linestyle='-.')
     plt.show()
     
-def subplots(y, x, nrows=None, ncols=1, yaxis=1, title=None, subtitle=None, xlabel='x', ylabel='y', grid=False, bgcolor='#D1DDC5', **kwargs):
+def subplots(y, x, nrows=None, ncols=1, yaxis=1, title=None, subtitle=None, \
+             xlabel='x', ylabel='y', grid=False, bgcolor='#D1DDC5', **kwargs):
     assert x.ndim == 1 and y.ndim == 2
     if yaxis == 0:
         y = np.swapaxes(y, 0, 1)
@@ -99,11 +102,42 @@ def plot_scale(y, x, xscale='log', yscale=None, xscale_kwargs=None, yscale_kwarg
         ax.grid(color='grey', linewidth='1', linestyle='-.')
     plt.show()
 
-def get_fscale(base=10, linthresh=10, linscale=10, subs=None):
-    # Frequency scale parameters for plotting frequency response, to be used in "plot_scale()".
-    # Please refer to "matplotlib.scale.SymmetricalLogScale"
-    # Returns: xscale, xscale_kwargs
-    return 'symlog', dict(base=base, linthresh=linthresh, subs=subs, linscale=linscale)
+def plot_frm(frm, f, base=10, linthresh=10, linscale=10, subs=None, \
+             delete_zero=True, grid=True, bgcolor='#D1DDC5', **kwargs):
+    # Plot frequency response: magnitude.
+    # base, linthresh linscale and subs are keyword arguments for 'matplotlib.scale.SymmetricalLogScale'.
+    # In case of zero division warning: (old_settings =) np.seterr(divide='ignore')
+    if delete_zero:
+        frm, f = frm[1:], f[1:]
+    frm = 20*np.log10(frm)
+    fig, ax = plt.subplots(facecolor=bgcolor)
+    ax.set_facecolor(bgcolor)
+    ax.set_xscale('symlog', base=base, linthresh=linthresh, subs=subs, linscale=linscale)
+    ax.plot(f, frm, **kwargs)
+    plt.title('frequency response: magnitude')
+    plt.xlabel(xlabel, loc='frequency (Hz)')
+    plt.ylabel(ylabel, loc='magnitude (dB)')
+    if grid:
+        ax.grid(color='grey', linewidth='1', linestyle='-.')
+    plt.show()
+
+def plot_frp(frp, f, base=10, linthresh=10, linscale=10, subs=None, \
+             delete_zero=False, grid=True, bgcolor='#D1DDC5', **kwargs):
+    # Plot frequency response: magnitude.
+    # base, linthresh linscale and subs are keyword arguments for 'matplotlib.scale.SymmetricalLogScale'.
+    # In case of zero division warning: (old_settings =) np.seterr(divide='ignore')
+    if delete_zero:
+        frp, f = frp[1:], f[1:]
+    fig, ax = plt.subplots(facecolor=bgcolor)
+    ax.set_facecolor(bgcolor)
+    ax.set_xscale('symlog', base=base, linthresh=linthresh, subs=subs, linscale=linscale)
+    ax.plot(f, frp, **kwargs)
+    plt.title('frequency response: phase')
+    plt.xlabel(xlabel, loc='frequency (Hz)')
+    plt.ylabel(ylabel, loc='phase (rad)')
+    if grid:
+        ax.grid(color='grey', linewidth='1', linestyle='-.')
+    plt.show()
 
 def plot_fr(fr, f, grid=True, bgcolor='#D1DDC5', **kwargs):
     # plot frequency response - magnitude.
@@ -119,7 +153,8 @@ def plot_fr(fr, f, grid=True, bgcolor='#D1DDC5', **kwargs):
         ax.grid(color='grey', linewidth='1', linestyle='-.')
     plt.show()
                 
-def plot_modes(Modes, t, au=None, res=None, compare_with_noise=True, title=None, xlabel='time', ylabel='magnitude', grid=False, bgcolor='#D1DDC5', **kwargs):
+def plot_modes(Modes, t, au=None, res=None, compare_with_noise=True, title=None, \
+               xlabel='time', ylabel='magnitude', grid=False, bgcolor='#D1DDC5', **kwargs):
     assert Modes.shape[1] == t.size
     assert Modes.shape[0] < Modes.shape[1]
     N = Modes.shape[0]
