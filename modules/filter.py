0bff75ec7613807ce737d330d155691bc513092a
fan455 <66545913+fan455@users.noreply.github.com>
2023-02-06 13:13:46 +0800

    Update filter.py

--- modules/filter.py
+++ modules/filter.py
@@ -12,65 +12,70 @@ from scipy import signal
 def iir(y, b, a, axis=0):
     return signal.lfilter(b, a, y, axis=axis)
 
-def iir2(y, b, a, axis=0):
-    return signal.filtfilt(b, a, y, axis=axis)
-
 def iirsos(y, sos, axis=0):
     return signal.sosfilt(sos, y, axis=axis)
-
-def iirsos2(y, sos, axis=0):
-    return signal.sosfiltfilt(sos, y, axis=axis)
 
 def butter(y, sr, btype, order, freq, axis=0):
     # N (lp or hp) or 2*N (bp or bs) -order Butterworth filter
     # btype: 'lowpass', 'highpass', 'bandpass', 'bandstop'
-    b, a = get_ba_butter(sr, btype, order, freq)
+    b, a = ba_butter(sr, btype, order, freq)
     return iir(y, b, a, axis=axis)
 
+def buttersos(y, sr, btype, order, freq, axis=0):
+    sos = sos_butter(sr, btype, order, freq)
+    return iirsos(y, sos, axis=axis)
+
+def bq(y, sr, sos_bq_type, axis=0, **kwargs):
+    # Single biquad filter.
+    # The type of sos_bq_type is FUNCTION, e.g. sos_bq_type=sos_bq_allpass.
+    # **kwargs need to be single values, e.g. freq=200, Q=0.7.
+    # **kwargs may include: freq, Q, bw (bandwidth), slope, gain. But only freq is always needed.
+    # Please refer to the different "sos_bq_type" functions in the "IIR filter coefficient" section.
+    sos = sos_bq(sr, sos_bq_type, **kwargs)
+    return irrsos(y, sos, axis=axis)
+
+def bqcas(y, sr, sos_bq_type, axis=0, **kwargs):
+    # Cascaded biquad filters of the same type.
+    # The type of sos_bq_type is FUNCTION, e.g. sos_bq_type=sos_bq_allpass.
+    # **kwargs need to be lists, e.g. freq=[200, 500, 1000], Q=[0.7, 1.0, 1.2].
+    # The length of input lists (should be the same) is the number of sos.
+    # Returned sos array shape is (number of sos, 6).
+    sos = sos_bqcas(sr, sos_bq_type, **kwargs)
+    return irrsos(y, sos, axis=axis)
+
+def bqtile(y, sr, sos_bq_type, ntile=2, axis=0, **kwargs):
+    # Tiled biquad filters of the same type and the same parameters.
+    # **kwargs need to be single values, e.g. freq=200, Q=0.7.
+    sos = sos_bq(sr, sos_bq_type, **kwargs)
+    sos = tile_sos(sos, ntile)
+    return irrsos(y, sos, axis=axis)
+
+def iir2(y, b, a, axis=0):
+    return signal.filtfilt(b, a, y, axis=axis)
+
+def iirsos2(y, sos, axis=0):
+    return signal.sosfiltfilt(sos, y, axis=axis)
+
 def butter2(y, sr, btype, order, freq, axis=0):
-    b, a = get_ba_butter(sr, btype, order, freq)
+    b, a = ba_butter(sr, btype, order, freq)
     return iir2(y, b, a, axis=axis)
 
-def buttersos(y, sr, btype, order, freq, axis=0):
-    sos = get_sos_butter(sr, btype, order, freq)
-    return iirsos(y, sos, axis=axis)
-
 def buttersos2(y, sr, btype, order, freq, axis=0):
-    sos = get_sos_butter(sr, btype, order, freq)
+    sos = sos_butter(sr, btype, order, freq)
     return iirsos2(y, sos, axis=axis)
 
-def bq(y, sr, bqfunc, freq, Q, gain=None, axis=0):
-    """
-    Single biquad filter
+def bq2(y, sr, sos_bq_type, axis=0, **kwargs):
+    sos = sos_bq(sr, sos_bq_type, **kwargs)
+    return irrsos2(y, sos, axis=axis)
 
-    Parameters:
-    y: ndarray. The signal to be filtered.
-    sr: positive int (Hz). Sample rate of y.
-    bqfunc: function. Which function to use to get the sos coefficients of biquad filter,
-        e.g. bq_lowpass. Please refer to the "IIR filter coefficient" section.
-    freq: positive float. The "significant frequency".
-    Q: positive float. Quality factor.
-    gain: float (dB). For peak, low shelf and high shelf filters only.
-    axis: int. Which axis of y to filter along.
+def bqcas2(y, sr, sos_bq_type, axis=0, **kwargs):
+    sos = sos_bqcas(sr, sos_bq_type, **kwargs)
+    return irrsos2(y, sos, axis=axis)
 
-    Returns:
-    y_filtered: ndarray. Filtered signal.
-    """
-    sos = bqfunc(sr, freq, Q, gain)
-    return iirsos(y, sos, axis=axis)
-
-def bq2(y, sr, bqfunc, freq, Q, gain=None, axis=0):
-    sos = bqfunc(sr, freq, Q, gain)
-    return iirsos2(y, sos, axis=axis)
-
-def bqcas(y, sr, bqfunc_list, freq_list, Q_list, gain_list=None, axis=0):
-    # Cascaded-sos biquad filter, with list inputs.
-    sos = get_sos_bqcas(sr, bqfunc_list, freq_list, Q_list, gain_list)
-    return iirsos(y, sos, axis=axis)
-
-def bqcas2(y, sr, bqfunc_list, freq_list, Q_list, gain_list=None, axis=0):
-    sos = get_sos_bqcas(sr, bqfunc_list, freq_list, Q_list, gain_list)
-    return iirsos2(y, sos, axis=axis)
+def bqtile2(y, sr, sos_bq_type, ntile=2, axis=0, **kwargs):
+    sos = sos_bq(sr, sos_bq_type, **kwargs)
+    sos = tile_sos(sos, ntile)
+    return irrsos2(y, sos, axis=axis)
 
 # IIR filter frequency response
 # Input: filter coefficients/parameters
@@ -85,22 +90,6 @@ def fr_iirsos(sr, sos):
     f, z = signal.sosfreqz(sos, fs=sr)
     return f, 20*np.log10(abs(z)), np.unwrap(np.angle(z))
 
-def fr_bq(sr, bqfunc, freq, Q, gain=None):
-    sos = bqfunc(sr, freq, Q, gain)
-    return fr_irrsos(sr, sos)
-
-def fr_bqsos(sr, bqfunc_list, freq_list, Q_list, gain_list=None):
-    sos = get_sos_bq(sr, bqfunc_list, freq_list, Q_list, gain_list)
-    return fr_irrsos(sr, sos)
-
-def fr_butter(sr, btype, order, freq, axis=0):
-    b, a = get_ba_butter(sr, btype, order, freq)
-    return fr_irr(sr, b, a)
-
-def fr_buttersps(sr, btype, order, freq, axis=0):
-    sos = get_sos_butter(sr, btype, order, freq)
-    return fr_irrsos(sr, sos)
-
 # IIR filter coefficient
 # Input: filter parameters
 # Output: filter coefficients
@@ -110,23 +99,23 @@ def cascade_sos(sos_list):
     # input sos list (or tuple): [sos1, sos2,..., sosn]
     return np.concatenate(sos_list, axis=0)
 
-def tile_sos(sos, n):
-    # Tile a sos from shape(nsos, 6) to shape(n*nsos, 6).
-    return np.tile(sos, (n, 1))
+def tile_sos(sos, ntile):
+    # Tile a sos from shape(nsos, 6) to shape(ntile*nsos, 6).
+    return np.tile(sos, (ntile, 1))
 
-def get_ba_butter(sr, btype, order, freq):
+def ba_butter(sr, btype, order, freq):
     # Get the b and a parameters of a butterworth IIR filter.
     # signal.butter
     # btype: 'lowpass', 'highpass', 'bandpass', 'bandstop'
     return signal.butter(order, freq, btype=btype, fs=sr)
 
-def get_sos_butter(sr, btype, order, freq):
+def sos_butter(sr, btype, order, freq):
     # Get the sos of a butterworth IIR filter.
     # signal.butter
     # btype: 'lowpass', 'highpass', 'bandpass', 'bandstop'
     return signal.butter(order, freq, btype=btype, output='sos', fs=sr)
 
-def get_sos_iir(sr, ftype, btype, order, freq, rp=None, rs=None):
+def sos_iir(sr, ftype, btype, order, freq, rp=None, rs=None):
     # Get the sos of certain types of IRR filter.
     # signal.iirfilter
     # ftype: 'butter', 'cheby1', 'cheby2', 'ellip', 'bessel'
@@ -134,28 +123,23 @@ def get_sos_iir(sr, ftype, btype, order, freq, rp=None, rs=None):
     return signal.iirfilter(order, freq, rp=rp, rs=rs, btype=btype, \
                             ftype=ftype, output='sos', fs=sr)
 
-def get_sos_bq(sr, bqfunc, freq, Q, gain=None):
+def sos_bq(sr, sos_bq_type, **kwargs):
     # Get the sos coefficient array of a single biquad filter.
-    return bqfunc(sr, freq, Q, gain)
+    return sos_bq_type(sr, **kwargs)
 
-def get_sos_bqcas(sr, bqfunc_list, freq_list, Q_list, gain_list=None):
-    # Get the sos coefficient array of cascaded biquad filters.
-    # Returned sos array shape is (number of sos, 6).
-    # The length of input lists (should be the same) is the number of sos.
-    nsos = len(freq_list)
-    if gain_list is None:
-        gain_list = [None]*nsos
+def sos_bqcas(sr, sos_bq_type, **kwargs):
+    # Get the sos coefficient array of cascaded biquad filters, limited to the same biquad type.
+    kwargs_key, kwargs_val = list(kwargs.keys()), list(kwargs.values())
+    nsos = len(kwargs_val[0])
     sos = np.empty((0, 6))
     for i in range(nsos):
-        bqfunc = bqfunc_list[i]
-        freq = freq_list[i]
-        Q = Q_list[i]
-        gain = gain_list[i]
-        sos_ = bqfunc(sr, freq, Q, gain)
+        subkwargs_val = list(sub_val[i] for sub_val in kwargs_val)
+        subkwargs = dict(zip(kwargs_key, subkwargs_val))
+        sos_ = sos_bq_type(sr, **subkwargs)
         sos = np.append(sos, sos_, axis=0)
     return sos
 
-def bq_allpass(sr, freq, Q, gain=None):
+def sos_bq_allpass(sr, freq, Q):
     # biquad all pass filter
     w = 2*np.pi*freq/sr
     cosw, sinw = np.cos(w), np.sin(w)
@@ -165,7 +149,7 @@ def bq_allpass(sr, freq, Q, gain=None):
     a = np.array([1.0, -2*cosw/norm, (1-alpha)/norm])
     return np.array([np.append(b, a)])
         
-def bq_lowpass(sr, freq, Q, gain=None):
+def sos_bq_lowpass(sr, freq, Q):
     # biquad low pass filter
     w = 2*np.pi*freq/sr
     cosw, sinw = np.cos(w), np.sin(w)
@@ -175,7 +159,7 @@ def bq_lowpass(sr, freq, Q, gain=None):
     a = np.array([1.0, -2*cosw/norm, (1-alpha)/norm])
     return np.array([np.append(b, a)])
 
-def bq_highpass(sr, freq, Q, gain=None):
+def sos_bq_highpass(sr, freq, Q):
     # biquad high pass filter
     w = 2*np.pi*freq/sr
     cosw, sinw = np.cos(w), np.sin(w)
@@ -185,53 +169,43 @@ def bq_highpass(sr, freq, Q, gain=None):
     a = np.array([1.0, -2*cosw/norm, (1-alpha)/norm])
     return np.array([np.append(b, a)])
 
-def bq_bandpass(sr, freq, Q, gain=None):
+def sos_bq_bandpass(sr, freq, bw):
     # biquad band pass filter with constant 0 dB peak gain.
     w = 2*np.pi*freq/sr
     cosw, sinw = np.cos(w), np.sin(w)
-    alpha = 0.5*sinw/Q
+    alpha = sinw*np.sinh(0.5*np.log(2)*bw*w/sinw)
     norm = 1+alpha    
     b = np.array([alpha, 0.0, -alpha])/norm
     a = np.array([1.0, -2*cosw/norm, (1-alpha)/norm])
     return np.array([np.append(b, a)])
 
-def bq_bandpass2(sr, freq, Q, gain=None):
-    # biquad band pass filter with constant Q dB peak gain.
-    w = 2*np.pi*freq/sr
-    cosw, sinw = np.cos(w), np.sin(w)
-    alpha = 0.5*sinw/Q
-    norm = 1+alpha    
-    b = np.array([Q*alpha, 0.0, -Q*alpha])/norm
-    a = np.array([1.0, -2*cosw/norm, (1-alpha)/norm])
-    return np.array([np.append(b, a)])
-
-def bq_bandstop(sr, freq, Q, gain=None):
+def sos_bq_bandstop(sr, freq, bw):
     # biquad band stop (notch) filter
     w = 2*np.pi*freq/sr
     cosw, sinw = np.cos(w), np.sin(w)
-    alpha = 0.5*sinw/Q
+    alpha = sinw*np.sinh(0.5*np.log(2)*bw*w/sinw)
     norm = 1+alpha    
     b = np.array([1.0, -2*cosw, 1.0])/norm
     a = np.array([1.0, -2*cosw/norm, (1-alpha)/norm])
     return np.array([np.append(b, a)])
     
-def bq_peak(sr, freq, Q, gain):
+def sos_bq_peak(sr, freq, bw, gain):
     # biquad peaking (bell) EQ
     w = 2*np.pi*freq/sr
     cosw, sinw = np.cos(w), np.sin(w)
-    alpha = 0.5*sinw/Q
+    alpha = sinw*np.sinh(0.5*np.log(2)*bw*w/sinw)
     A = np.power(10, gain/40)
     norm = 1+alpha/A    
     b = np.array([1+alpha*A, -2*cosw, 1-alpha*A])/norm
     a = np.array([1.0, -2*cosw/norm, (1-alpha/A)/norm])
     return np.array([np.append(b, a)])
 
-def bq_lowshelf(sr, freq, Q, gain):
+def sos_bq_lowshelf(sr, freq, slope, gain):
     # biquad low shelf
     w = 2*np.pi*freq/sr
     cosw, sinw = np.cos(w), np.sin(w)
-    alpha = 0.5*sinw/Q
     A = np.power(10, gain/40)
+    alpha = 0.5*sinw*np.sqrt((A+1/A)*(1/slope-1)+2)
     norm = (A+1) + (A-1)*cosw + 2*np.sqrt(A)*alpha    
     b0 = A*((A+1) - (A-1)*cosw + 2*np.sqrt(A)*alpha)
     b1 = 2*A*((A-1) - (A+1)*cosw)
@@ -242,12 +216,12 @@ def bq_lowshelf(sr, freq, Q, gain):
     a = np.array([1.0, a1/norm, a2/norm])
     return np.array([np.append(b, a)])
 
-def bq_highshelf(sr, freq, Q, gain):
+def sos_bq_highshelf(sr, freq, slope, gain):
     # biquad high shelf
     w = 2*np.pi*freq/sr
     cosw, sinw = np.cos(w), np.sin(w)
-    alpha = 0.5*sinw/Q
     A = np.power(10, gain/40)
+    alpha = 0.5*sinw*np.sqrt((A+1/A)*(1/slope-1)+2)
     norm = (A+1) - (A-1)*cosw + 2*np.sqrt(A)*alpha    
     b0 = A*((A+1) + (A-1)*cosw + 2*np.sqrt(A)*alpha)
     b1 = -2*A*((A-1) + (A+1)*cosw)
