35c81f7b8dd6c54b76cbd983ae99749522c22daf
fan455 <66545913+fan455@users.noreply.github.com>
2023-02-24 21:36:56 +0800

    Update math.py

--- modules/math.py
+++ modules/math.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy.linalg import lstsq
 from scipy.interpolate import CubicSpline, PchipInterpolator
 
 def itp(x, px, py, axis=-1):
@@ -8,7 +7,44 @@ def itp(x, px, py, axis=-1):
 def itpmono(x, px, py, axis=-1):
     return PchipInterpolator(px, py, axis=axis)(x)
 
-def ols1v(px, py):
-    M = np.vstack((np.ones(px.size), px)).T
-    p, res, rnk, s = lstsq(M, py)
-    return p[0], p[1]
+def ols0v(y):
+    """
+    Parameters
+    y: 1d array of shape (nsamples,)
+
+    Returns
+    cons: float. The intercept of fitted line.
+    res: float. The sum of squared residuals.
+    """
+    (cons,), (res,) = np.linalg.lstsq(np.ones((y.size, 1)), y, rcond=None)[:2]
+    return cons, res
+
+def ols1v(y, x):
+    """
+    Parameters
+    y: 1d array of shape (nsamples,)
+    x: 1d array of shape (nsamples,)
+
+    Returns
+    cons: float. The intercept of fitted line.
+    coef: float. The coefficient of x.
+    res: float. The sum of squared residuals.
+    """
+    coef, (res,) = np.linalg.lstsq(np.vstack([np.ones(x.size), x]).T, \
+                                   y, rcond=None)[:2]
+    return coef[0], coef[1], res
+    
+def olsmv(y, X):
+    """
+    Parameters
+    y: 1d array of shape (nsamples,)
+    X: 2d array of shape (nsamples, nx)
+
+    Returns
+    cons: float. The intercept of fitted line.
+    coef: 1d array of shape (nx,). The coefficients of X.
+    res: float. The sum of squared residuals.
+    """
+    coef, (res,) = np.linalg.lstsq(np.append(np.ones((y.size, 1)), X, axis=1), \
+                                   y, rcond=None)[:2]
+    return coef[0], coef[1:], res
