0a02aa2e9769ba764ed502365f7ab6bbb179aa9c
fan455 <66545913+fan455@users.noreply.github.com>
2023-03-28 18:49:41 +0800

    Update plot.py

--- modules/plot.py
+++ modules/plot.py
@@ -9,142 +9,163 @@ import matplotlib.pyplot as plt
 #plt.rcParams['font.sans-serif'] = ['SimHei']
 #plt.rcParams['axes.unicode_minus'] = False
 #plt.rcParams.update({'font.size': 11.5})
+#plt.style.use('seaborn')
 
-def plot(y, x=None, title='title', xlabel='x', ylabel='y', xtick=None, ytick=None, \
+# The default blue color is 'tab:blue' or '#1f77b4'
+
+def plot(y, x=None, yaxis=1, title='title', xlabel='x', ylabel='y', xtick=None, ytick=None, \
          xticklabel=None, yticklabel=None, xtick_kwargs=None, ytick_kwargs=None, \
-         grid=True, bgcolor='#D1DDC5', **kwargs):
-    # x and y are both 1d arrays with the same size.
+         grid=True, bgcolor='#d1ddc5', **kwargs):
+    # x is 1d array, y can be 1d or 2d array.
+    # If y is 2d, please notice that by default the second axis of y is plotted.    
+    if y.ndim == 2:
+        if yaxis == 1:
+            y = np.swapaxes(y, 0, 1)        
+    if x is None:
+        x = np.arange(y.shape[0])
+
     fig, ax = plt.subplots(facecolor=bgcolor)
     ax.set_facecolor(bgcolor)
-    if x is None:
-        ax.plot(y, **kwargs)
-    else:
-        ax.plot(x, y, **kwargs)
-
+    
+    ax.plot(x, y, **kwargs)
+    
     if xtick is not None:
         ax.set_xticks(xtick)
         if xticklabel is not None:
             ax.set_xticklabels(xticklabel)
-        if xtick_kwargs is not None:
-            ax.tick_params(axis='x', **xtick_kwargs)
+        if xtick_kwargs is None:
+            xtick_kwargs = {}
+        ax.tick_params(axis='x', **xtick_kwargs)
 
     if ytick is not None:
         ax.set_yticks(ytick)
         if yticklabel is not None:
             ax.set_yticklabels(yticklabel)
-        if ytick_kwargs is not None:
-            ax.tick_params(axis='y', **ytick_kwargs)
-
+        if ytick_kwargs is None:
+            ytick_kwargs = {}
+        ax.tick_params(axis='y', **ytick_kwargs)
+        
+    if grid:
+        ax.grid(color='grey', linewidth='0.75', linestyle='-.')
+        
     plt.title(title)
     plt.xlabel(xlabel, loc='right')
     plt.ylabel(ylabel, loc='center')
-    if grid:
-        ax.grid(color='grey', linewidth='0.75', linestyle='-.')
     plt.show()
 
-def plot_multi(y, x, yaxis=1, linelabels=None, kwargslist=None, legendkwargs=None, \
+def plot_multi(y, x=None, yaxis=1, linelabels=None, legendkwargs=None, \
                title='title', xlabel='x', ylabel='y', xtick=None, ytick=None, \
                xticklabel=None, yticklabel=None, xtick_kwargs=None, ytick_kwargs=None, \
-               grid=True, bgcolor='#D1DDC5'):
+               grid=True, bgcolor='#d1ddc5', kwargslist=None, **kwargs):
     # Plot multiple lines in the same graph.
     # x is a 1d array with shape (npoints,).
     # y is a 2d array with shape (N, npoints) if yaxis=1, or shape (npoints, N) if yaxis=0.
     # kwargslist is a list containing N dictionaries with kwargs sent to ax.plot() corresponding to the N lines.
+    # Please notice that if both kwargslist and **kwargs is not None, they should not have any intersection.
     # linelabels is a list containing N strings setting the labels of lines.
     # legendkwargs is a dictionary of legend parameters, sent to ax.legend()
     if yaxis == 0:
         y = np.swapaxes(y, 0, 1)
-    N = y.shape[0]
+    N = y.shape[0]        
+    if x is None:
+        x = np.arange(y.shape[1])        
+    if linelabels is None:
+        linelabels = list(f'y{i}' for i in range(N))        
+    if kwargslist is None:
+        kwargslist = [{}] * N
+        
     fig, ax = plt.subplots(facecolor=bgcolor)
     ax.set_facecolor(bgcolor)
     
-    if linelabels is None:
-        linelabels = list(f'y{i}' for i in range(N))
-        
-    if kwargslist:
-        for i in range(N):
-            ax.plot(x, y[i, :], **kwargslist[i])
-    else:
-        for i in range(N):
-            ax.plot(x, y[i, :])
+    for i in range(N):
+        ax.plot(x, y[i, :], **kwargslist[i], **kwargs)
             
-    if legendkwargs:
-        ax.legend(linelabels, **legendkwargs)
-    else:
-        ax.legend(linelabels)
+    if legendkwargs is None:
+        legendkwargs = {}
+    ax.legend(linelabels, **legendkwargs)
 
     if xtick is not None:
         ax.set_xticks(xtick)
         if xticklabel is not None:
             ax.set_xticklabels(xticklabel)
-        if xtick_kwargs is not None:
-            ax.tick_params(axis='x', **xtick_kwargs)
+        if xtick_kwargs is None:
+            xtick_kwargs = {}
+        ax.tick_params(axis='x', **xtick_kwargs)
 
     if ytick is not None:
         ax.set_yticks(ytick)
         if yticklabel is not None:
             ax.set_yticklabels(yticklabel)
-        if ytick_kwargs is not None:
-            ax.tick_params(axis='y', **ytick_kwargs)
-
-    plt.title(title)
-    plt.xlabel(xlabel, loc='right')
-    plt.ylabel(ylabel, loc='center')
+        if ytick_kwargs is None:
+            ytick_kwargs = {}
+        ax.tick_params(axis='y', **ytick_kwargs)
+        
     if grid:
         ax.grid(color='grey', linewidth='0.75', linestyle='-.')
+        
+    plt.title(title)
+    plt.xlabel(xlabel, loc='right')
+    plt.ylabel(ylabel, loc='center')    
     plt.show()
     
-def subplots(y, x, nrows=None, ncols=1, yaxis=1, title=None, subtitle=None, \
-             xlabel='x', ylabel='y', grid=False, bgcolor='#D1DDC5', **kwargs):
+def subplots(y, x=None, nrows=None, ncols=1, yaxis=1, title=None, subtitle=None, \
+             xlabel='x', ylabel='y', grid=False, bgcolor='#d1ddc5', kwargslist=None, **kwargs):
     # x is a 1d array, y is a 2d array with shape(n_subplots, x.size).
     if yaxis == 0:
         y = np.swapaxes(y, 0, 1)
+    N = y.shape[0]    
     if nrows is None:
-        nrows = y.shape[0]
-    N = y.shape[0]
+        nrows = y.shape[0]        
+    if x is None:
+        x = np.arange(y.shape[1])
+    if kwargslist is None:
+        kwargslist = [{}] * N
+        
     fig, ax = plt.subplots(nrows, ncols, facecolor=bgcolor)
     ax = ax.reshape(ax.size)
+        
     if subtitle:
         if isinstance(subtitle, str):
             for i in range(N):
                 ax[i].set_title(f'{subtitle} {i+1}', fontsize='medium')     
                 ax[i].set_facecolor(bgcolor)
-                ax[i].plot(x, y[i, :], **kwargs)
+                ax[i].plot(x, y[i, :], **kwargslist[i], **kwargs)
         elif isinstance(subtitle, (list, tuple)):
             assert len(subtitle) == N
             for i in range(N):
                 ax[i].set_title(subtitle[i], fontsize='medium')     
                 ax[i].set_facecolor(bgcolor)
-                ax[i].plot(x, y[i, :], **kwargs)
+                ax[i].plot(x, y[i, :], **kwargslist[i], **kwargs)
         else:
             raise ValueError('Your subtitle type {type(subtitle)} is not supported.')
     else:
         for i in range(N):
             ax[i].set_facecolor(bgcolor)
-            ax[i].plot(x, y[i, :], **kwargs)
+            ax[i].plot(x, y[i, :], **kwargslist[i], **kwargs)
 
-    nEmpty = nrows*ncols - N
+    nEmpty = nrows * ncols - N
     if nEmpty > 0:
         for i in range(nEmpty):
-            ax[-nEmpty].set_facecolor(bgcolor)
-            
+            ax[-nEmpty].set_facecolor(bgcolor)            
     if grid:
         for i in range(N):
-            ax[i].grid(color='grey', linewidth='0.75', linestyle='-.')
+            ax[i].grid(color='grey', linewidth='0.75', linestyle='-.')            
     if title:
         fig.suptitle(title)
+        
     plt.xlabel(xlabel, loc='right')
     plt.ylabel(ylabel, loc='center')
     plt.show()
 
-def plot_sc(y, x, x_trtime, yaxis=1, linelabels=['actual', 'synthetic'], \
-            kwargslist=[{}, dict(linestyle='--',color='green')], \
+def plot_sc(y, x, x_trtime, yg=None, yaxis=1, ygaxis=1, linelabels=['actual','synthetic'], \
+            kwargslist=[{}, dict(linestyle='--', color='green')], \
+            ygkwargs=dict(linestyle='--', color='grey'), \
             vlinekwargs=dict(color='grey', lw=2.0, ls='--'), \
             legendkwargs=dict(loc='lower left'), title='synthetic control', \
             xlabel='time', ylabel='y', xtick=None, ytick=None, \
             xticklabel=None, yticklabel=None, \
             xtick_kwargs=dict(labelsize=9, labelrotation=45), ytick_kwargs=None, \
-            grid=True, bgcolor='#D1DDC5'):
+            grid=True, bgcolor='#d1ddc5', **kwargs):
     # Plot actual and synthetic control lines in the same graph.
     # x is a 1d array with shape (npoints,).
     # y is a 2d array with shape (N, npoints) if yaxis=1, or shape (npoints, N) if yaxis=0.
@@ -153,92 +174,110 @@ def plot_sc(y, x, x_trtime, yaxis=1, linelabels=['actual', 'synthetic'], \
     # linelabels is a list containing N strings setting the labels of lines.
     # legendkwargs is a dictionary of legend parameters, sent to ax.legend()    
     if yaxis == 0:
-        y = np.swapaxes(y, 0, 1)
-    N = y.shape[0]
+        y = np.swapaxes(y, 0, 1)        
+    N = y.shape[0]    
+    if linelabels is None:
+        linelabels = list(f'y{i}' for i in range(N))        
+    if kwargslist is None:
+        kwargslist = [{}] * N
+        
     fig, ax = plt.subplots(facecolor=bgcolor)
     ax.set_facecolor(bgcolor)
     
-    if linelabels is None:
-        linelabels = list(f'y{i}' for i in range(N))
-        
-    if kwargslist:
-        for i in range(N):
-            ax.plot(x, y[i, :], **kwargslist[i])
-    else:
-        for i in range(N):
-            ax.plot(x, y[i, :])
-            
-    if legendkwargs:
-        ax.legend(linelabels, **legendkwargs)
-    else:
-        ax.legend(linelabels)
+    for i in range(N):
+        ax.plot(x, y[i, :], **kwargslist[i], **kwargs)
+
+    if yg is not None:
+        if ygaxis == 1:
+            yg = np.swapaxes(yg, 0, 1)
+        if ygkwargs is None:
+            ygkwargs = {}
+        ax.plot(x, yg, **ygkwargs, **kwargs)
+
+    if legendkwargs is None:
+        legendkwargs = {}
+    ax.legend(linelabels, **legendkwargs)
 
     if xtick is not None:
         ax.set_xticks(xtick)
         if xticklabel is not None:
             ax.set_xticklabels(xticklabel)
-        if xtick_kwargs is not None:
-            ax.tick_params(axis='x', **xtick_kwargs)
+        if xtick_kwargs is None:
+            xtick_kwargs = {}
+        ax.tick_params(axis='x', **xtick_kwargs)
 
     if ytick is not None:
         ax.set_yticks(ytick)
         if yticklabel is not None:
             ax.set_yticklabels(yticklabel)
-        if ytick_kwargs is not None:
-            ax.tick_params(axis='y', **ytick_kwargs)
-        
+        if ytick_kwargs is None:
+            ytick_kwargs = {}
+        ax.tick_params(axis='y', **ytick_kwargs)
+
+    if vlinekwargs is None:
+        vlinekwargs = {}
     plt.axvline(x_trtime-1, **vlinekwargs)
+    
+    if grid:
+        ax.grid(color='grey', linewidth='0.75', linestyle='-.')
+        
     plt.title(title)
     plt.xlabel(xlabel, loc='right')
     plt.ylabel(ylabel, loc='center')
-    if grid:
-        ax.grid(color='grey', linewidth='0.75', linestyle='-.')
     plt.show()
  
 def plot_itp(y, x, points, title='title', xlabel='x', ylabel='y', \
-             grid=True, bgcolor='#D1DDC5', **kwargs):
+             grid=True, bgcolor='#d1ddc5', \
+             pointskwargs=dict(linestyle='', marker='.', markersize=9, mec='black', mfc='black'), \
+             **kwargs):
     # Plot for interpolation.
     # points is a 2d array with shape(n_points, 2), that will be annotated.
     # points = np.array([[x1, y1], [x2, y2],..., [xn, yn]])
-    fig, ax = plt.subplots(facecolor=bgcolor)
-    ax.set_facecolor(bgcolor)
-    ax.plot(x, y, **kwargs)
     n_points = points.shape[0]
     px, py = points[:, 0], points[:, 1]
-    ax.plot(px, py, linestyle='', marker='.', markersize=9, mec='black', mfc='black')
+
+    if pointskwargs is None:
+        pointskwargs = {}
+        
+    fig, ax = plt.subplots(facecolor=bgcolor)
+    ax.set_facecolor(bgcolor)    
+    ax.plot(x, y, **kwargs)
+    ax.plot(px, py, **pointskwargs, **kwargs)
+
+    if grid:
+        ax.grid(color='grey', linewidth='0.75', linestyle='-.')
+        
     plt.title(title)
     plt.xlabel(xlabel, loc='right')
     plt.ylabel(ylabel, loc='center')
-    if grid:
-        ax.grid(color='grey', linewidth='0.75', linestyle='-.')
     plt.show()
 
 def plot_scale(y, x, xscale='log', yscale=None, xscale_kwargs=None, yscale_kwargs=None, \
-               title='title', xlabel='x', ylabel='y', grid=True, bgcolor='#D1DDC5', **kwargs):
+               title='title', xlabel='x', ylabel='y', grid=True, bgcolor='#d1ddc5', **kwargs):
     # Plot with x and/or y axis scaled. By default x is scaled by "log10".
     # The types of xscale_kwargs and yscale_kwargs should be "dict", so use the "dict()" function.
     # Please refer to "matplotlib.scale".
     # xscale or yscale: 'asinh', 'function', 'functionlog', 'linear', 'log', 'logit', 'symlog'
     fig, ax = plt.subplots(facecolor=bgcolor)
     ax.set_facecolor(bgcolor)
+    
     if xscale is not None:
         if xscale_kwargs is None:
-            ax.set_xscale(xscale)
-        else:
-            ax.set_xscale(xscale, **xscale_kwargs)
+            xscale_kwargs = {}
+        ax.set_xscale(xscale, **xscale_kwargs)
     if yscale is not None:
         if yscale_kwargs is None:
-            ax.set_yscale(yscale)
-        else:
-            ax.set_yscale(yscale, **yscale_kwargs)
+            yscale_kwargs = {}
+        ax.set_yscale(yscale, **yscale_kwargs)
     ax.plot(x, y, **kwargs)
+
+    if grid:
+        ax.grid(color='grey', linewidth='0.75', linestyle='-.')
+        
     plt.title(title)
     plt.xlabel(xlabel, loc='right')
     plt.ylabel(ylabel, loc='center')
-    if grid:
-        ax.grid(color='grey', linewidth='0.75', linestyle='-.')
     plt.show()
-
 
 def plot_fscale(y, x, faxis='x', base=10, linthresh=10, linscale=0.25, subs=None, \
                 title='title', xlabel='frequency (Hz)', ylabel='magnitude', \
